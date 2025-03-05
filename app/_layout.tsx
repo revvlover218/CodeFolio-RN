493aa00039a740b0544d61d9570f93debb3bce16
Reshal Luchman <reshal.luchman@ayoba.me>
2025-03-05 12:34:36 +0200

    Fix android full white status bar in dark mode.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,24 @@
-import { Stack } from 'expo-router/stack';
+import { Stack } from "expo-router/stack";
+import * as Notifications from "expo-notifications";
+import { useColorScheme } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function Layout() {
+  const theme = useColorScheme();
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style={theme === "dark" ? "light" : "dark"} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
